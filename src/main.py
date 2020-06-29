bddd88c29a991ef77b36f0036ca60b24f6235997
Maximo Machado <maximo@mit.edu>
2020-06-29 17:29:30 -0400

    Fix algorithm to search through all playlists user saves.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import spotipy
 import spotipy.util as util
 
-# TODO Make git repository and start thinking how to format this
 # First make command line program, then expand to application
 # Allow choice of permissions to read private playlists or not
 # Function to format search query correctly and allow advanced searching
@@ -11,28 +10,37 @@ import spotipy.util as util
 
 def find_song_in_playlists(song_uri):
     # TODO Make it so all playlists gone through, method has limit to how many shown at a time
+    found_playlist_ids = set()
+
     playlists = sp.current_user_playlists()
+
+    find_song_helper(song_uri, playlists, found_playlist_ids)
+    while playlists['next']:
+        playlists = sp.next(playlists)
+        find_song_helper(song_uri, playlists, found_playlist_ids)
+
+    return found_playlist_ids
+
+def find_song_helper(song_uri, playlists, set_to_modify):
     for playlist in playlists['items']:
-        # print(playlist['name'])
         tracks = sp.playlist_tracks(playlist['id'])
 
         if is_track_in_tracks(song_uri, tracks):
-            print(f"Found in Playlist {playlist['name']}")
+            set_to_modify.add(playlist['id'])
         while tracks['next']:
             tracks = sp.next(tracks)
             if is_track_in_tracks(song_uri, tracks):
-                print(f"Found in Playlist {playlist['name']}")
-
+                set_to_modify.add(playlist['id'])
 
 def is_track_in_tracks(song_uri, tracks):
     """
     Checks whether song is within a list of songs
     :param song_uri: ID of target song
     :param tracks: Page object of list of tracks
-    :return: @hether or a not a song is within a list of tracks
+    :return: Whether or a not a song is within a list of tracks
     """
-    for track in tracks['items']:
-        if song_uri == track['track']['uri']:
+    for playlist_track in tracks['items']:
+        if song_uri == playlist_track['track']['uri']:
             return True
     return False
 
