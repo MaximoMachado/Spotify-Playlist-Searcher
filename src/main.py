f2806fb5c0b7dff88335cd2478dbf51687406d65
Maximo Machado <maximo@mit.edu>
2020-07-13 23:40:38 -0400

    Fix layout.

--- src/main.py
+++ src/main.py
@@ -7,14 +7,15 @@ root.title('Spotify Playlist Searcher')
 header = tk.Label(root, text="Spotify Playlist Searcher")
 header.grid(row=0, column=0, columnspan=2)
 
+
 settings_btn = tk.Button(root, text="Settings")
 settings_btn.grid(row=0, column=1, sticky=tk.E)
 
 search_bar = tk.Entry(root, width=50)
-search_bar.grid(row=1, column=0, sticky=tk.W)
+search_bar.grid(row=1, column=0)
 
-search_bar_submit = tk.Button(root, text="Submit")
-search_bar.grid(row=1, column=1, sticky=tk.W)
+search_bar_submit = tk.Button(root, text="Search for Song")
+search_bar_submit.grid(row=1, column=1)
 
 root.mainloop()
 
