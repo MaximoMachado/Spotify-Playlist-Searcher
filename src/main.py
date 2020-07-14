a2cb8fd4e28749ec3d5867371af22fada3482ea0
Maximo Machado <maximo@mit.edu>
2020-07-14 00:40:33 -0400

    Transition to app as class

--- src/main.py
+++ src/main.py
@@ -2,41 +2,46 @@ from src.spotipy_manager import *
 import tkinter as tk
 import os
 
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.create_base_widgets()
 
-def login_spotify():
-    spm = SpotipyManager()
-    load_widgets()
-    login_btn.grid_forget()
+    def create_base_widgets(self):
+        self.header = tk.Label(root, text="Spotify Playlist Searcher", width=50)
+        self.header.grid(row=0, column=0, columnspan=2, pady=10)
 
-def load_widgets():
-    search_bar = tk.Entry(root, width=50)
-    search_bar.grid(row=1, column=0)
-    search_bar.focus()
+        self.settings_btn = tk.Button(root, text="Settings")
+        self.settings_btn.grid(row=0, column=1, sticky=tk.E)
 
-    search_bar_submit = tk.Button(root, text="Search for Song", command=search_submit)
-    search_bar_submit.grid(row=1, column=1)
+        if os.path.exists("./data/.cache-user"):
+            self.spm = SpotipyManager()
+            self.load_widgets()
+        else:
+            self.login_btn = tk.Button(root, text="Login to Spotify", command=self.login_spotify)
+            self.login_btn.grid(row=1, column=0, columnspan=2)
 
-def search_submit():
-    search = search_bar.get()
-    print(spm.get_spotipy_client().search(search))
+    def login_spotify(self):
+        self.spm = SpotipyManager()
+        self.load_widgets()
+        self.login_btn.grid_forget()
+
+    def load_widgets(self):
+        self.search_bar = tk.Entry(root, width=50)
+        self.search_bar.grid(row=1, column=0)
+        self.search_bar.focus()
+
+        self.search_bar_submit = tk.Button(root, text="Search for Song", command=self.search_submit)
+        self.search_bar_submit.grid(row=1, column=1)
+
+    def search_submit(self):
+        search = self.search_bar.get()
+        print(self.spm.get_spotipy_client().search(search))
+
 
 root = tk.Tk()
 root.title('Spotify Playlist Searcher')
-
-header = tk.Label(root, text="Spotify Playlist Searcher", width=50)
-header.grid(row=0, column=0, columnspan=2, pady=10)
-
-settings_btn = tk.Button(root, text="Settings")
-settings_btn.grid(row=0, column=1, sticky=tk.E)
-
-spm = None
-if os.path.exists("./data/.cache-user"):
-    spm = SpotipyManager()
-    load_widgets()
-else:
-    login_btn = tk.Button(root, text="Login to Spotify", command=login_spotify)
-    login_btn.grid(row=1, column=0, columnspan=2)
-
-
-root.mainloop()
+app = Application(master=root)
+app.mainloop()
 
