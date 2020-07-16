240ac4b2fc6342103d4e5167d097edb6c086c5ca
Maximo Machado <maximo@mit.edu>
2020-07-16 19:04:49 -0400

    Implement song_dict. Add state change on listbox select for playlist button.

--- src/main.py
+++ src/main.py
@@ -28,19 +28,24 @@ class Application(tk.Frame):
 
         self.search_results = tk.Listbox(self.main_frame, width=50)
         self.search_results.grid(row=2, column=0, columnspan=2, padx=5, pady=10)
+        self.search_results.bind('<<ListboxSelect>>', lambda x: self.check_song_selection())
 
-        self.playlist_search_btn = tk.Button(self.main_frame, text="Search Playlists For Song", state=tk.DISABLED)
+        self.playlist_search_btn = tk.Button(self.main_frame, text="Search Playlists For Song", command=self.search_playlists, state=tk.DISABLED)
         self.playlist_search_btn.grid(row=3, column=0, columnspan=2, pady=5)
 
     def search_submit(self):
+        # Disables playlist button and clears search results
         self.search_results.delete(0, tk.END)
+        self.playlist_search_btn['state'] = tk.DISABLED
 
+        self.song_dict = {}  # Maps how song appears in listbox to the song's uri for playlist searching
         search = self.search_bar.get()
         if search:
             paging_object = self.spm.get_spotipy_client().search(search)
             tracks = paging_object['tracks']['items']
 
             for track in tracks:
+
                 artists = track['artists']
                 artists_str = ''
                 for i, artist in enumerate(artists):
@@ -48,7 +53,18 @@ class Application(tk.Frame):
                     if not i == len(artists) - 1:
                         artists_str += ', '
 
-                self.search_results.insert(tk.END, f"{track['name']}    -   {artists_str}")
+                output_str = f"{track['name']}    -   {artists_str}"
+                self.search_results.insert(tk.END, output_str)
+                self.song_dict[output_str] = track['uri']
+
+    def check_song_selection(self):
+        if self.search_results.curselection():
+            self.playlist_search_btn['state'] = tk.NORMAL
+        else:
+            self.playlist_search_btn['state'] = tk.DISABLED
+
+    def search_playlists(self):
+        pass
 
 
 
