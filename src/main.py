a143aa0a703f67ab42c5c37cfca44d0f9c1a9879
Maximo Machado <maximomachado246@gmail.com>
2020-07-17 18:17:09 -0400

    Playlist search (#5)
    * Add playlist search button. Bind enter to song search. Minor Fixes.
    
    * Implement song_dict. Add state change on listbox select for playlist button.
    
    * Implement passing of selected song to search_playlists.
    
    * Implement playlist listbox showing results of search within the playlists.

--- src/main.py
+++ src/main.py
@@ -9,9 +9,14 @@ class Application(tk.Frame):
         self.main_frame = tk.Frame(master)
         self.main_frame.grid(row=0, column=0, columnspan=2, padx=10)
         self.spm = SpotipyManager()
+
         self.create_base_widgets()
 
     def create_base_widgets(self):
+        # Defining shared widget settings
+        listbox_width = 50
+
+        # GUI widgets
         self.header = tk.Label(self.main_frame, text="Spotify Playlist Searcher", width=50)
         self.header.grid(row=0, column=0, columnspan=2, pady=10)
 
@@ -21,30 +26,70 @@ class Application(tk.Frame):
         self.search_bar = tk.Entry(self.main_frame, width=50)
         self.search_bar.grid(row=1, column=0)
         self.search_bar.focus()
+        self.search_bar.bind('<Return>', lambda x: self.search_submit())
 
         self.search_bar_submit = tk.Button(self.main_frame, text="Search for Song", command=self.search_submit)
         self.search_bar_submit.grid(row=1, column=1)
 
-        self.search_results = tk.Listbox(width=50)
+        # TODO Add 'Song Results:' to start of listbox and make it unselectable.
+        self.search_results = tk.Listbox(self.main_frame, width=listbox_width)
         self.search_results.grid(row=2, column=0, columnspan=2, padx=5, pady=10)
+        self.search_results.bind('<<ListboxSelect>>', lambda x: self.check_song_selection())
+
+        self.playlist_search_btn = tk.Button(self.main_frame, text="Search Playlists For Song", command=self.search_playlists, state=tk.DISABLED)
+        self.playlist_search_btn.grid(row=3, column=0, columnspan=2, pady=5)
+
+        # Will be displayed at later point
+        self.playlist_results = tk.Listbox(self.main_frame, width=listbox_width)
 
     def search_submit(self):
-        # TODO Bind function to enter key, either while in focus of search bar or anytime at all.
+        # Disables playlist button and clears search results
         self.search_results.delete(0, tk.END)
+        self.playlist_search_btn['state'] = tk.DISABLED
 
+        self.song_dict = {}  # Maps how song appears in listbox to the song's uri for playlist searching
         search = self.search_bar.get()
-        paging_object = self.spm.get_spotipy_client().search(search)
-        tracks = paging_object['tracks']['items']
+        if search:
+            paging_object = self.spm.get_spotipy_client().search(search)
+            tracks = paging_object['tracks']['items']
 
-        for track in tracks:
-            artists = track['artists']
-            artists_str = ''
-            for i, artist in enumerate(artists):
-                artists_str += f'{artist["name"]}'
-                if not i == len(artists) - 1:
-                    artists_str += ', '
+            for track in tracks:
 
-            self.search_results.insert(tk.END, f"{track['name']}    -   {artists_str}")
+                artists = track['artists']
+                artists_str = ''
+                for i, artist in enumerate(artists):
+                    artists_str += f'{artist["name"]}'
+                    if not i == len(artists) - 1:
+                        artists_str += ', '
+
+                output_str = f"{track['name']}    -    {artists_str}"
+                self.search_results.insert(tk.END, output_str)
+                self.song_dict[output_str] = track['uri']
+
+    def check_song_selection(self):
+        if self.search_results.curselection():
+            self.playlist_search_btn['state'] = tk.NORMAL
+        else:
+            self.playlist_search_btn['state'] = tk.DISABLED
+
+    def search_playlists(self):
+
+        song_selected = self.search_results.selection_get()
+        song_uri = self.song_dict[song_selected]
+        playlist_uris = self.spm.find_song_in_playlists(song_uri)
+        playlist_names = [self.spm.get_name_from_uri(uri) for uri in playlist_uris]
+
+        # Displaying playlist listbox and then inserting playlists
+        self.playlist_results.grid(row=4, column=0, columnspan=2, padx=5, pady=10)
+        self.playlist_results.delete(0, tk.END)
+        if playlist_names:
+            for name in playlist_names:
+                self.playlist_results.insert(tk.END, name)
+        else:
+            self.playlist_results.insert(tk.END, 'The selected song is not found in any of your playlists.')
+
+
+
 
 
 root = tk.Tk()
