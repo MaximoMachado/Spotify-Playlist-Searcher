373abc726a6e7c75700234e12f286c8b9c6f6ebb
Maximo Machado <maximo@mit.edu>
2020-07-22 21:59:41 -0400

    Implement saving of settings choices in ui to self.settings.

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ class Application(tk.Frame):
         self.main_frame = tk.Frame(master)
         self.main_frame.grid(row=0, column=0, columnspan=2, padx=10)
         self.spm = SpotipyManager()
-        self.settings = {'threading': False, 'playlists_exclude': []}
+        self.settings = {'cache': False, 'playlists_exclude': ()}
 
         self.create_base_widgets()
 
@@ -137,6 +137,7 @@ class Application(tk.Frame):
     def create_settings_widgets(self):
         self.settings_window = tk.Toplevel(self.master)
         self.settings_window.title('Settings')
+        self.settings_window.protocol("WM_DELETE_WINDOW", self.exit_settings)
 
         self.settings_frame = tk.Frame(self.settings_window)
         self.settings_frame.grid(row=0, column=0)
@@ -147,8 +148,8 @@ class Application(tk.Frame):
         self.region_label = tk.Label(self.settings_frame, text='Select Spotify Region')
         self.region_label.grid(row=1, column=0)
 
-        cache_val = tk.BooleanVar()
-        self.cache_toggle = tk.Checkbutton(self.settings_frame, variable=cache_val,
+        self.cache_val = tk.BooleanVar()
+        self.cache_toggle = tk.Checkbutton(self.settings_frame, variable=self.cache_val,
                                            text='Enable Caching (Inaccurate results if the playlist have been modified recently)')
         self.cache_toggle.grid(row=1, column=0, sticky=tk.W)
 
@@ -162,14 +163,22 @@ class Application(tk.Frame):
 
             check_val = tk.BooleanVar()
             self.check_vals.append((check_val, f'{playlist["uri"]}'))
-            option = tk.Checkbutton(self.playlist_options_frame, text=playlist_name, variable=self.check_vals[i][0], command=lambda: self.print_val())
+            option = tk.Checkbutton(self.playlist_options_frame, text=playlist_name, variable=self.check_vals[i][0])
             option.grid(row=i, column=0, columnspan=2, sticky=tk.W)
 
-    def print_val(self):
-        print([val[0].get() for val in self.check_vals])
+    def exit_settings(self):
+        """
+        Saves settings to self.settings before exiting.
+        """
+        self.settings['cache'] = self.cache_val.get()
+        self.settings['playlists_exclude'] = (check_val[1] for check_val in self.check_vals if check_val[0].get())
+        print(self.settings)
+        self.settings_window.destroy()
 
-root = tk.Tk()
-root.title('Spotify Playlist Searcher')
-app = Application(master=root)
-app.mainloop()
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title('Spotify Playlist Searcher')
+    app = Application(master=root)
+    app.mainloop()
 
